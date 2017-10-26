b997692bdf5584cb4f08d41d11ce662bd7226c47
Wladimir Guerra <wladimir.guerra@gmail.com>
2017-10-26 10:12:34 -0200

    licence update; template added for future implementations

--- templates/_test/bgp-convergence-broken-link.sh
+++ templates/_test/bgp-convergence-broken-link.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+#
+# This script test and record logs of update from bgpd
+# simulating a broken link
+#
+
+scriptName="${0##*/}"
+
+declare -i DEFAULT_TIMEOUT=30
+declare -i DEFAULT_INTERVAL=30
